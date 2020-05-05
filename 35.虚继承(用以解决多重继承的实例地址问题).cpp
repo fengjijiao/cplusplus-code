9e004fa3645ef03cff743a51b1840f5b0791b3ae
JiJiao Feng <16521693+fengjijiao@users.noreply.github.com>
2020-05-06 06:30:11 +0800

    Create 35.虚继承(用以解决多重继承的实例地址问题).cpp

---
+++ 35.虚继承(用以解决多重继承的实例地址问题).cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class CBase {
+public:
+    string id;
+};
+//因为虚继承，CBase类在此不产生副本
+class CDerive1 : virtual public CBase {
+public:
+    void show1() {
+        cout << "CDerive1: " << id << endl;
+    }
+};
+//因为虚继承，CBase类在此不产生副本
+class CDerive2 : virtual public CBase {
+public:
+    void show2() {
+        cout << "CDerive2: " << id << endl;
+    }
+};
+class CSon : public CDerive2, public CDerive1 { };
+int main ( )
+{
+    CSon s;
+    s.CDerive1::id = "WD8503026";
+    s.CDerive2::id = "WD8503027";
+    s.CBase::id = "WD8503025";
+    s.show1();
+    s.show2();
+    cout << "BASE: " << s.CBase::id << endl;
+    return 0;
+}
+//理论上新版c++也无法执行
+/*
+CDerive1: WD8503025
+CDerive2: WD8503025
+BASE: WD8503025
+*/
