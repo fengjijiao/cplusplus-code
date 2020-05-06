f4ca9f894175c738fb40b220a10fe01a176c079e
JiJiao Feng <16521693+fengjijiao@users.noreply.github.com>
2020-05-06 13:17:39 +0800

    Update 56.静态成员函数.cpp

--- 56.静态成员函数.cpp
+++ 56.静态成员函数.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 class Integer {
 public:
+    const int a = 10;
     static int atoi(const char *s) {
         return ::atoi(s);//:: = std ::
     }
@@ -11,12 +12,18 @@ public:
 };
 int main ( )
 {
+    Integer *integer = new Integer();
+    cout << integer->a << endl;
+    //cout << Integer::a << endl; Integer::a不是静态的无法通过类名直接访问
     int x = Integer::atoi("322");
     float y = Integer::atof("3.14");
     cout << "x = " << x << "  y = " << y << endl;
     return 0;
 }
-//x = 322  y = 3.14
+/*
+10
+ x = 322  y = 3.14
+ */
 /*
 按Java的经验，静态成员函数一般在公共程序中用的比较多。这样的程序与类自身关系不大，只是归类性质放在某个类中。还有一些与类转换或交换但不需要实例时使用的情况。
  */
